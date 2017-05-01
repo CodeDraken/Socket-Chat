894e37a0439920f48a23c87c09a12a4d4a0ce0ba
CodeDraken <codedraken@gmail.com>
2017-05-01 15:06:46 -0400

    Setup SocketIO

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
 // static file server
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
 });
